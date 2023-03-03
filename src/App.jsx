df99137879efc48c214b4782650908eb6b6c2331
LeeTaeKyung <leek9708@gmail.com>
2023-03-03 21:09:27 +0900

    useEffect 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,32 @@
-import React, { useState } from "react";
-
-const heavyWork = () => {
-  console.log("무거운 작업!");
-  return ["홍길동", "김민수"];
-};
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [names, setNames] = useState(() => {
-    return heavyWork();
-  }); // 무거운 작업일때는 초기값에 콜백함수를 넣기
-
-  const [input, setInput] = useState("");
-  const handleInputChange = (e) => {
-    return setInput(e.target.value);
-  };
-  // const clickAddbtn = () => {
-  //   // const newNames = [input, ...names];
-  //   setNames([input, ...names]);
-  // };
-
-  const clickAddbtn = () => {
-    return setNames((아무이름) => {
-      return [input, ...아무이름];
+  const [count, setCount] = useState(1);
+  const [name, setName] = useState("");
+  const handleCountUpdate = () => {
+    return setCount((count) => {
+      return count + 1;
     });
   };
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+  // 의존성 배열을 안써주면 렌더링 될때마다(=스테이트가 업데이트 될 때마다) 매번 실행됨
+  // 마운트 될 때 + 의존성배열이 변경될때만 실행됨
+  useEffect(() => {
+    console.log("name 변화");
+  }, [name]); //의존성배열
+
+  // useEffect(() => {
+  //   console.log("name 변화");
+  // }, []); // 이 코드는 마운트 될 때 만 실행됩니다.
 
   return (
     <div>
-      <input type="text" value={input} onChange={handleInputChange} />
-      <button
-        onClick={() => {
-          clickAddbtn();
-          // return clickAddbtn(), clickAddbtn(), clickAddbtn();
-        }}
-      >
-        upload
-      </button>{" "}
-      {names.map((name, i) => {
-        return <p key={i}>{name}</p>;
-      })}
+      <button onClick={handleCountUpdate}>Update</button>
+      <span>count : {count}</span>
+      <input type="text" value={name} onChange={handleInputChange} />
+      <span> name : {name}</span>
     </div>
   );
 }
