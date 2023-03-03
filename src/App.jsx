07a22cb3cd9037ad2864d82c90c4ab149250cc50
LeeTaeKyung <leek9708@gmail.com>
2023-03-03 13:41:32 +0900

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,42 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const countRef = useRef(0);
-  const style = {
-    border: "1px solid black",
-    margin: "10px",
-    padding: "10px",
-  };
-  const plusStateCountBtnHandler = () => {
-    setCount(count + 1);
-  };
-  const plusRefCountBtnHandler = () => {
-    countRef.current++;
-    console.log(countRef.current);
-  };
+  const [id, setId] = useState("");
+  const idRef = useRef("");
+  const pwRef = useRef("");
+  //화면이 렌더링 될 때, 어떤 작업을 하고 싶다 ! : useEffect !!!! r
+  useEffect(() => {
+    idRef.current.focus();
+    console.log(pwRef); // current가 input값으로 바뀜
+    console.log("랜더링 됐어요");
+  }, []);
+
+  // useEffect(() => {
+  //   if (id.length >= 10) {
+  //     return pwRef.current.focus();
+  //   }
+  // }, [id]);
 
   return (
     <>
-      <div style={style}>
-        {" "}
-        state 영역입니다. {count} <br />
-        <button onClick={plusStateCountBtnHandler}>state 증가</button>
+      <div>
+        아이디 :{" "}
+        <input
+          type="text"
+          ref={idRef}
+          value={id}
+          onChange={(e) => {
+            setId(e.target.value);
+            // 리액트에서 state 업데이트 방식은 배치 업데이트
+            // 내가 궁금해했던 하나씩 늦는 이유 -> 배치 업데이트
+            if (id.length >= 10) {
+              pwRef.current.focus();
+            }
+          }}
+        />
       </div>
-      <div style={style}>
-        ref 영역입니다. {countRef.current} <br />
-        <button onClick={plusRefCountBtnHandler}>ref증가</button>
+      <div>
+        비밀번호 : <input type="password" ref={pwRef} />
       </div>
     </>
   );
